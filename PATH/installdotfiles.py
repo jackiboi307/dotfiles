84f48a75b18464574c725efdfe6b4c5ff11eaa66
jack <jackiboi307@fakemail.com>
2025-10-10 18:26:49 +0200

    new install script

---
+++ PATH/installdotfiles.py
@@ -0,0 +1,39 @@
+#!/bin/python
+
+import os
+
+input("""Make sure you are in a directory such as ~/dotfiles
+Proceed by pressing enter, cancel by pressing Ctrl-C """)
+
+def cmd(command):
+    print(command)
+    os.system(command)
+
+simple = (
+    (".bashrc", "~"),
+    (".profile", "~"),
+    ("colors.env", "~"),
+    ("commands.sh", "~"),
+)
+
+overwrite = [
+    ("PATH", "~"),
+    ("Bar", "~"),
+]
+
+overwrite_subdirs = (
+    ".config",
+)
+
+for name in overwrite_subdirs:
+    for subdir in os.listdir(name):
+        # TODO check if directory?
+        overwrite.append((name + "/" + subdir, "~/" + name))
+
+for src, dest in simple:
+    cmd(f"ln -f {src} {dest}")
+
+for src, dest in overwrite:
+    cmd(f"rm -rf ~/{src}")
+    cmd(f"cp -rl {src} {dest}")
+
